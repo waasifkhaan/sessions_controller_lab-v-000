41fea5efbad515ed189cbd752355feaf9288684f
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 03:26:26 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-    
+
 
   end
 
   def create
     if params[:name].nil?
       redirect_to sessions_new_path
-    
+    else 
+    end
+  end 
+
 
   end
 
