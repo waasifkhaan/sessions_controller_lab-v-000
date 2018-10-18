edeb660b205dbb4905ba51a63a782c311de6c45f
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 03:41:18 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+
+
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+
+
+  def destroy
+    if session[:name]
+      session[:name] = nil
+    end
+    redirect_to root_path
+  end
+end
